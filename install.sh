0526f41224c31a6d6d1ae6748149dbca2420c868
jdbranham <jdbranham@hotmail.com>
2016-02-23 13:33:46 -0600

    renamed files/variables removed netcat dependency

--- install.sh
+++ install.sh
@@ -7,31 +7,25 @@ MONITOR_INTERVAL=$3
 GRAPHITE_PORT=$4
 DEFAULT_GRAPHITE_PORT="2003"
 
-CONFIG="$INSTALL_DIR/quickstatd.conf"
-PID_FILE="$INSTALL_DIR/.quickstatd.pid"
+CONFIG="$INSTALL_DIR/shellstatd.conf"
+PID_FILE="$INSTALL_DIR/.shellstatd.pid"
 INIT_DIR="$INSTALL_DIR"
 
 # check for root
 if [ $UID == "0" ]
 then
-   CONFIG="/etc/quickstatd.conf"
-   PID_FILE="/var/run/.quickstatd.pid"
+   CONFIG="/etc/shellstatd.conf"
+   PID_FILE="/var/run/.shellstatd.pid"
    INIT_DIR="/etc/init.d"
 fi
 
 # check for current directory
 if ! ls awk/vmstat.awk > /dev/null 2>&1
 then
-   echo "Installer must be run from the quickstatd directory."
+   echo "Installer must be run from the shellstatd directory."
    exit 1
 fi
 
-# check for netcat
-if ! which nc > /dev/null
-then
-   echo "FATAL: netcat ('nc') not installed."
-   exit 1
-fi
 
 if [ "$MONITOR_INTERVAL" == "" ]
 then
@@ -44,7 +38,7 @@ then
    GRAPHITE_PORT=$DEFAULT_GRAPHITE_PORT
 fi
 
-echo "Installing quickstatd."
+echo "Installing shellstatd."
 echo "   INSTALL_DIR: $INSTALL_DIR"
 echo "   GRAPHITE_HOST: $GRAPHITE_HOST"
 echo "   MONITOR_INTERVAL: $MONITOR_INTERVAL"
@@ -53,15 +47,15 @@ echo "   CONFIG: $CONFIG"
 echo "   PID_FILE: $PID_FILE"
 echo "   INIT_DIR: $INIT_DIR"
 
-echo "QUICKSTATD_HOME=\"$INSTALL_DIR\"" > $CONFIG
+echo "SHELLSTATD_HOME=\"$INSTALL_DIR\"" > $CONFIG
 echo "PID_FILE=\"$PID_FILE\"" >> $CONFIG
 echo >> $CONFIG
 
 # copy bits to new dir
 cp -r awk $INSTALL_DIR
 cp -r metric.d $INSTALL_DIR
-cat conf/quickstatd.conf >> $CONFIG
-cp quickstatd $INIT_DIR
+cat conf/shellstatd.conf >> $CONFIG
+cp shellstatd $INIT_DIR
 
 echo "graphite_host=\"$GRAPHITE_HOST\"" >> $CONFIG
 echo "graphite_port=\"$GRAPHITE_PORT\"" >> $CONFIG
@@ -74,6 +68,6 @@ echo "graphite_interval_seconds=\"$MONITOR_INTERVAL\"" >> $CONFIG
 #   echo "         monitoring tools will be unavailable."
 #fi
 
-echo "quickstatd installed successfully to $INSTALL_DIR"
+echo "shellstatd installed successfully to $INSTALL_DIR"
 
 
