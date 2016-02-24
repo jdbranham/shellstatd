b29bddeae9558cc513d8729f29b405d2a1be7a5d
jdbranham <jdbranham@hotmail.com>
2016-02-23 18:03:41 -0600

    test

--- metric.d/df.sh
+++ metric.d/df.sh
@@ -11,7 +11,7 @@ function repeat {
 CONFIG_FILE=$1
 . $CONFIG_FILE
 HOSTNAME=$(hostname -s)
-. ../graphite_send.sh
+. $SHELLSTATD_HOME/graphite_send.sh
 
 echo "Launching df monitoring, reporting data to $graphite_host"
 repeat "df -hP | gawk -f $SHELLSTATD_HOME/awk/df.awk hostname=$HOSTNAME"
