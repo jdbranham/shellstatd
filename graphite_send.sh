618aaf7b85abca862ac9850110f0869b94105b2e
jdbranham <jdbranham@hotmail.com>
2016-02-28 02:01:46 -0600

    added graphite_protocol variable

--- graphite_send.sh
+++ graphite_send.sh
@@ -1,5 +1,5 @@
 function sendToGraphite { 
   while IFS= read -r line; do
-    echo "$line" > /dev/tcp/$graphite_host/$graphite_port
+    echo "$line" > /dev/$graphite_protocol/$graphite_host/$graphite_port
   done
 }
