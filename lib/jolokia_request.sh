6a6994b6e472bada589cde3854c31324be61bd3b
jdbranham <jdbranham@hotmail.com>
2016-03-03 17:07:46 -0600

    re

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -26,6 +26,6 @@ function extractMBeanName {
    echo -e "First - $MBEAN\n"
    MBEAN="${MBEAN/$MATCH_BEAN/$HOSTNAME}" 
    echo -e "Second: $MBEAN\n"
-   echo $MBEAN | sed -rn 's/ //g' | printf
+   echo $MBEAN | sed -rn 's/ //g' | awk '{ print $1 }'
    # $MBEAN=${MBEAN/\'MATCH_BEANTYPE\'/\.}
 }
