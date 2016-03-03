93c3d2bf477a1398c89d61e399d6f92e0f7c8fa9
jdbranham <jdbranham@hotmail.com>
2016-03-03 16:17:41 -0600

    re

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -23,7 +23,7 @@ function extractMBeanName {
    local MATCH_BEAN='\["request","mbean"\]'
    local MATCH_BEANTYPE='\:type\='
    local MBEAN="`egrep $MATCH_BEAN $TEMP_FILE`"
-   MBEAN="${MBEAN/$MATCH_BEAN/$HOSTNAME} | sed 's|$MATCH_BEANTYPE|'"
+   MBEAN="${MBEAN/$MATCH_BEAN/$HOSTNAME}" | sed 's|$MATCH_BEANTYPE|' | sed 's|[\", ]||'
    echo $MBEAN
    # $MBEAN=${MBEAN/\'MATCH_BEANTYPE\'/\.}
 }
