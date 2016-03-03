e24625e3388785f95dc91a0f0d35288f8dff7983
jdbranham <jdbranham@hotmail.com>
2016-03-03 15:18:13 -0600

    re

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -9,8 +9,8 @@ function jolokiaRequest {
        # do something with ${LINE}
        curl -s ${LINE} | $SHELLSTATD_HOME/lib/JSON.sh -b > $TEMP_FILE | egrep '\["value"\]'
        local MBEAN=(egrep $MATCH_BEAN $TEMP_FILE)
-       $MBEAN=${$MBEAN/$MATCH_BEAN/$HOSTNAME}
-       $MBEAN=${$MBEAN/$MATCH_BEANTYPE/\.}
+       $MBEAN=${MBEAN/MATCH_BEAN/HOSTNAME}
+       $MBEAN=${MBEAN/MATCH_BEANTYPE/\.}
        echo $MBEAN
    done
    # close file
