21327bf89ee1f661a25304d836adfcc1bc52c15a
jdbranham <jdbranham@hotmail.com>
2016-03-03 15:43:32 -0600

    re

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -1,19 +1,26 @@
+# Requires JSON.sh
+
 function jolokiaRequest { 
    # open file for read, assign descriptor
    exec 3<$1
-   TEMP_FILE=(mktemp)
-   MATCH_BEAN='\["request","mbean"\]'
-   MATCH_BEANTYPE='\:type\='
    while read -u 3 LINE
    do
-       # do something with ${LINE}
-       curl -s ${LINE} | $SHELLSTATD_HOME/lib/JSON.sh -b > $TEMP_FILE | egrep '\["value"\]'
-       local MBEAN=`egrep $MATCH_BEAN $TEMP_FILE`
-       echo $MBEAN
-       $MBEAN=${MBEAN/\'\["request","mbean"\]\'/$HOSTNAME}
-       $MBEAN=${MBEAN/\'MATCH_BEANTYPE\'/\.}
-       echo $MBEAN
+      local TEMP_FILE=(mktemp)
+      # do something with ${LINE}
+      curl -s ${LINE} | $SHELLSTATD_HOME/lib/JSON.sh -b > $TEMP_FILE
+      local MBEAN_NAME=extractMBeanName $TEMP_FILE
+      rm $TEMP_FILE
+      echo $MBEAN_NAME
    done
    # close file
    exec 3<&-
+}
+
+function extractMBeanName {
+   local TEMP_FILE=$1
+   local MATCH_BEAN='\["request","mbean"\]'
+   local MATCH_BEANTYPE='\:type\='
+   local MBEAN=`egrep $MATCH_BEAN $TEMP_FILE`
+   $MBEAN=${MBEAN/request/$HOSTNAME}
+   # $MBEAN=${MBEAN/\'MATCH_BEANTYPE\'/\.}
 }
