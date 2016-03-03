7e3a0561018276920834ce775de27df6cfe055e9
jdbranham <jdbranham@hotmail.com>
2016-03-03 13:11:40 -0600

    refactor

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -1,11 +1,11 @@
 function jolokiaRequest { 
    # open file for read, assign descriptor
-   exec {3}<$1     
-   while read -u ${3} LINE
+   exec 3<$1     
+   while read -u 3 LINE
    do
        # do something with ${LINE}
        echo ${LINE}
    done
    # close file
-   exec {3}<&-    
+   exec 3<&-    
 }
