e0f363ff634ad0d6b303d2ae85a3cf58a8326aae
jdbranham <jdbranham@hotmail.com>
2016-03-04 00:21:06 -0600

    re

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -26,15 +26,14 @@ function jolokiaRequest {
          ''|*[!0-9]*) 
             while read -r value_entry
             do
-               PAYLOAD+=("$MBEAN_NAME $MBEAN_ATTRIBUTE $value_entry $MBEAN_TIMESTAMP\n")
+               PAYLOAD+=("$MBEAN_NAME $MBEAN_ATTRIBUTE $value_entry $MBEAN_TIMESTAMP")
             done < <(echo "$MBEAN_VALUE") ;;
          # The value must be a number
          *) 
-            PAYLOAD+=("$MBEAN_NAME $MBEAN_ATTRIBUTE $MBEAN_VALUE $MBEAN_TIMESTAMP\n") ;;
+            PAYLOAD+=("$MBEAN_NAME $MBEAN_ATTRIBUTE $MBEAN_VALUE $MBEAN_TIMESTAMP") ;;
       esac
-      while read -r payload_entry; do
-         echo $payload_entry
-      done < <(echo -e "$PAYLOAD")
+      for payload_item in $(PAYLOAD[*]); do
+         printf "%s\n" $payload_item
       #echo -e $PAYLOAD
    done
    # close file
