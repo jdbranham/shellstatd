ca7016f6fcbbf2c5d0b2f6ca567f5990dad48f2d
jdbranham <jdbranham@hotmail.com>
2016-03-03 18:20:01 -0600

    re

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -11,6 +11,9 @@ function jolokiaRequest {
       # do something with ${LINE}
       curl -s ${LINE} | $SHELLSTATD_HOME/lib/JSON.sh -b > $TEMP_FILE
       local MBEAN_NAME=`extractMBeanName $TEMP_FILE`
+      local MBEAN_ATTRIBUTE=`extractMBeanAttribute $TEMP_FILE`
+      local MBEAN_VALUE=`extractMBeanValue $TEMP_FILE`
+      local MBEAN_TIMESTAMP=`extractMBeanTimestamp $TEMP_FILE`
       rm $TEMP_FILE
       echo -e "$MBEAN_NAME\n"
    done
@@ -29,4 +32,28 @@ function extractMBeanName {
    echo -e "Second: $MBEAN\n"
    echo $MBEAN | sed -r 's/[\ ]//g;s/[=,\"]/./g' 
    # $MBEAN=${MBEAN/\'MATCH_BEANTYPE\'/\.}
+}
+
+function extractMBeanAttribute {
+   local TEMP_FILE=$1
+   local MATCH_BEAN='\["request","attribute"\]'
+   local MBEAN="`egrep $MATCH_BEAN $TEMP_FILE`"
+   echo $MBEAN | awk '{print $2}'
+}
+
+function extractMBeanValue {
+   local TEMP_FILE=$1
+   local MATCH_BEAN='\["value"'
+   local MBEAN="`egrep $MATCH_BEAN $TEMP_FILE`"
+   MBEAN="${MBEAN//'value'/}" 
+   for m in $( MBEAN ); do
+      echo $m | sed 's/[\[,\],\"]//g' awk '{print $1, $2}'
+   done
+}
+
+function extractMBeanTimestamp {
+   local TEMP_FILE=$1
+   local MATCH='\["timestamp"\]'
+   local MBEAN="`egrep $MATCH_BEAN $TEMP_FILE`"
+   echo $MBEAN | awk '{ print $2 }'
 }
