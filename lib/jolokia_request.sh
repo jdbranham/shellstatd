dd73dc31dbcefefdaad0af33397f5f96612a042b
jdbranham <jdbranham@hotmail.com>
2016-03-03 22:12:13 -0600

    re

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -15,10 +15,10 @@ function jolokiaRequest {
       local MBEAN_VALUE=`extractMBeanValue $TEMP_FILE`
       local MBEAN_TIMESTAMP=`extractMBeanTimestamp $TEMP_FILE`
       rm $TEMP_FILE
-      echo -e "MBEAN_NAME: $MBEAN_NAME\n"
-      echo -e "MBEAN_ATTRIBUTE: $MBEAN_ATTRIBUTE\n"
-      echo -e "MBEAN_VALUE: $MBEAN_VALUE\n"
-      echo -e "MBEAN_TIMESTAMP: $MBEAN_TIMESTAMP\n"
+      #echo -e "MBEAN_NAME: $MBEAN_NAME\n"
+      #echo -e "MBEAN_ATTRIBUTE: $MBEAN_ATTRIBUTE\n"
+      #echo -e "MBEAN_VALUE: $MBEAN_VALUE\n"
+      #echo -e "MBEAN_TIMESTAMP: $MBEAN_TIMESTAMP\n"
       
       local PAYLOAD=()
       case $MBEAN_VALUE in
@@ -32,6 +32,7 @@ function jolokiaRequest {
          *) 
             PAYLOAD+=("$MBEAN_NAME $MBEAN_ATTRIBUTE $MBEAN_VALUE $MBEAN_TIMESTAMP") ;;
       esac
+      echo $PAYLOAD
    done
    # close file
    exec 3<&-
