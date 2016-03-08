c8c5d8e376354d71e703c5a7dc97c2bd98c43bce
jdbranham <jdbranham@hotmail.com>
2016-03-08 12:47:43 -0600

    added logging

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
 # Requires JSON.sh
-
 function jolokiaRequest { 
-   # open file for read, assign descriptor
-   exec 3<$1
-   while read -u 3 LINE
-   do
-      local PREFIX=$graphite_prefix
-      local TEMP_FILE=(mktemp)
-      curl -s ${LINE} | $SHELLSTATD_HOME/lib/JSON.sh -b -n > $TEMP_FILE
-      local MBEAN_NAME=`extractMBeanName $TEMP_FILE`
-      local MBEAN_ATTRIBUTE=`extractMBeanAttribute $TEMP_FILE`
-      local MBEAN_VALUE=`extractMBeanValue $TEMP_FILE`
-      local MBEAN_TIMESTAMP=`extractMBeanTimestamp $TEMP_FILE`
-      rm $TEMP_FILE
-      #echo -e "MBEAN_NAME: $MBEAN_NAME\n"
-      #echo -e "MBEAN_ATTRIBUTE: $MBEAN_ATTRIBUTE\n"
-      #echo -e "MBEAN_VALUE: $MBEAN_VALUE\n"
-      #echo -e "MBEAN_TIMESTAMP: $MBEAN_TIMESTAMP\n"
-      
-      local PAYLOAD=()
-      local regexNumber='^[0-9]+$'
-      if [[ "$MBEAN_VALUE" =~ $regexNumber ]]; then
-         # The value is a number
-         local full_string="$PREFIX$MBEAN_NAME$MBEAN_ATTRIBUTE $MBEAN_VALUE $MBEAN_TIMESTAMP\n"
-         PAYLOAD+=("${full_string}") 
-      else
-         while read -r value_entry; do
-            if [ ! "$value_entry" = "" ]; then
-               local full_string="$PREFIX$MBEAN_NAME$MBEAN_ATTRIBUTE.$value_entry $MBEAN_TIMESTAMP\n"
-               PAYLOAD+=("${full_string}")
-            fi
-         done < <(echo -e "$MBEAN_VALUE")
-      fi
-      for payload_item in "${PAYLOAD[*]}"; do
-         echo -e "$payload_item"
-      done
-   done
-   # close file
-   exec 3<&-
+	# open file for read, assign descriptor
+	exec 3<$1
+	while read -u 3 LINE
+	do
+		local PREFIX=$graphite_prefix
+		local TEMP_FILE=(mktemp)
+		curl -s ${LINE} | $SHELLSTATD_HOME/lib/JSON.sh -b -n > $TEMP_FILE
+		local MBEAN_NAME=`extractMBeanName $TEMP_FILE`
+		local MBEAN_ATTRIBUTE=`extractMBeanAttribute $TEMP_FILE`
+		local MBEAN_VALUE=`extractMBeanValue $TEMP_FILE`
+		local MBEAN_TIMESTAMP=`extractMBeanTimestamp $TEMP_FILE`
+		rm $TEMP_FILE
+		if [ -z $verbose_logging ]; then
+			echo -e "MBEAN_NAME: $MBEAN_NAME\n" >> $LOG
+			echo -e "MBEAN_ATTRIBUTE: $MBEAN_ATTRIBUTE\n" >> $LOG
+			echo -e "MBEAN_VALUE: $MBEAN_VALUE\n" >> $LOG
+			echo -e "MBEAN_TIMESTAMP: $MBEAN_TIMESTAMP\n" >> $LOG
+		fi
+		
+	      
+		local	PAYLOAD=()
+		local regexNumber='^[0-9]+$'
+		if [[ "$MBEAN_VALUE" =~ $regexNumber ]]; then
+			# The value is a number
+			local full_string="$PREFIX$MBEAN_NAME$MBEAN_ATTRIBUTE $MBEAN_VALUE $MBEAN_TIMESTAMP\n"
+			PAYLOAD+=("${full_string}") 
+		else
+			while read -r value_entry; do
+			if [ ! "$value_entry" = "" ]; then
+				local full_string="$PREFIX$MBEAN_NAME$MBEAN_ATTRIBUTE.$value_entry $MBEAN_TIMESTAMP\n"
+				PAYLOAD+=("${full_string}")
+			fi
+			done < <(echo -e "$MBEAN_VALUE")
+		fi
+		for payload_item in "${PAYLOAD[*]}"; do
+			echo -e "$payload_item"
+		done
+	done
+	# close file
+	exec 3<&-
 }
 
 function extractMBeanName {
