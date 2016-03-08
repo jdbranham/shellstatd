06d4cdf8bcbf618ec8ab0c76c99995e42bb1e2a6
jdbranham <jdbranham@hotmail.com>
2016-03-08 12:20:51 -0600

    enabled graphite send for jolokia module

--- test/test_sendToGraphite.sh
+++ test/test_sendToGraphite.sh
@@ -12,19 +12,19 @@ SHELLSTATD_HOME=`pwd`
 . $SHELLSTATD_HOME/lib/graphite_send.sh
 
 
-echo -e "#### Testing udp with bash ####\n"
+echo -e "\n\n#### Testing udp with bash ####\n"
 cat $1 | sendToGraphite
 
-echo -e "#### Testing udp with python ####\n"
+echo -e "\n\n#### Testing udp with python ####\n"
 use_python_send=true
 cat $1 | sendToGraphite
 
-echo -e "#### Testing tcp with bash ####\n"
+echo -e "\n\n#### Testing tcp with bash ####\n"
 use_python_send=false
 graphite_port=2004
 graphite_protocol=tcp
 cat $1 | sendToGraphite
 
-echo -e "#### Testing tcp with python ####\n"
+echo -e "\n\n#### Testing tcp with python ####\n"
 use_python_send=true
 cat $1 | sendToGraphite
