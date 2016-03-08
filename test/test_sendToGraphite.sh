91a7c1f58dc13fc992e69cc8ab892b88ebfe5166
jdbranham <jdbranham@hotmail.com>
2016-03-08 12:16:27 -0600

    added test cases

--- test/test_sendToGraphite.sh
+++ test/test_sendToGraphite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -e "Run from SHELLSTATD_HOME"
-echo -e "Testing udp\n"
+echo -e "NOTE: Run from SHELLSTATD_HOME\n"
+echo -e "Requires a TCP and UDP server running locally\n"
 
 use_python_send=false
 graphite_protocol=udp
@@ -12,4 +12,19 @@ SHELLSTATD_HOME=`pwd`
 . $SHELLSTATD_HOME/lib/graphite_send.sh
 
 
+echo -e "#### Testing udp with bash ####\n"
+cat $1 | sendToGraphite
+
+echo -e "#### Testing udp with python ####\n"
+use_python_send=true
+cat $1 | sendToGraphite
+
+echo -e "#### Testing tcp with bash ####\n"
+use_python_send=false
+graphite_port=2004
+graphite_protocol=tcp
+cat $1 | sendToGraphite
+
+echo -e "#### Testing tcp with python ####\n"
+use_python_send=true
 cat $1 | sendToGraphite
