d2c22063a9f8b217cea1694d257bd0f77c551ecc
jdbranham <jdbranham@hotmail.com>
2016-03-08 13:29:41 -0600

    syn

--- lib/graphite_send.sh
+++ lib/graphite_send.sh
@@ -1,14 +1,14 @@
 function sendToGraphite { 
-	if [ -z $use_python_send ]; then
+	if [ $use_python_send == true ]; then
 		if [  $verbose_logging == "true"  ]; then
-			echo -e "sendToGraphite: Using python send\n"
+			echo -e "sendToGraphite: Using python send\n" >> $LOG
 			echo -e $1 >> $LOG
 		fi
   		echo -e $1 | $SHELLSTATD_HOME/lib/python_send.py --server=$graphite_host --port=$graphite_port --protocol=$graphite_protocol
   	else
 		while IFS= read -r line; do
 			if [  $verbose_logging == "true"  ]; then
-				echo -e "sendToGraphite: Using bash send\n"
+				echo -e "sendToGraphite: Using bash send\n" >> $LOG
 				echo -e $1 >> $LOG
 			fi
 			echo "$line" > /dev/$graphite_protocol/$graphite_host/$graphite_port
