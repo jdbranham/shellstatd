fa43dedb134948d67fc27d3ec677797d5791183e
jdbranham <jdbranham@hotmail.com>
2016-03-08 15:47:01 -0600

    added PID for python_send

--- lib/graphite_send.sh
+++ lib/graphite_send.sh
@@ -3,7 +3,8 @@ function sendToGraphite {
 		if [  $verbose_logging == "True"  ]; then
 			echo -e "sendToGraphite: Using python send." >> $LOG
 		fi
-  		cat $1 | $SHELLSTATD_HOME/lib/python_send.py --server=$graphite_host --port=$graphite_port --protocol=$graphite_protocol --verbose=$verbose_logging
+  		cat $1 | $SHELLSTATD_HOME/lib/python_send.py --server=$graphite_host --port=$graphite_port --protocol=$graphite_protocol --verbose=$verbose_logging &
+  		echo $! >> $PID_FILE 
   	else
 		while IFS= read -r line; do
 			if [  $verbose_logging == "True"  ]; then
