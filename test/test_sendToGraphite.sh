d6ebd50293db9bb6a74da40d29a7ee3f88cd1cae
jdbranham <jdbranham@hotmail.com>
2016-03-07 18:29:16 -0600

    updated paths

--- test/test_sendToGraphite.sh
+++ test/test_sendToGraphite.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+echo -e "Run from SHELLSTATD_HOME"
 echo -e "Testing udp\n"
 
 use_python_send=false
 graphite_protocol=udp
 graphite_host=127.0.0.1
 graphite_port=2003
+SHELLSTATD_HOME=`pwd`
 
-. ../lib/graphite_send.sh
+. $SHELLSTATD_HOME/lib/graphite_send.sh
 
 cat example.txt | sendToGraphite
