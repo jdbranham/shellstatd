090e713be3a780b541d6b10452cc08e0e8907246
jdbranham <jdbranham@hotmail.com>
2016-03-09 13:16:24 -0600

    removed python_send from background

--- lib/graphite_send.sh
+++ lib/graphite_send.sh
@@ -3,8 +3,7 @@ function sendToGraphite {
 		if [  $verbose_logging == "True"  ]; then
 			echo -e "sendToGraphite: Using python send." >> $LOG
 		fi
-  		cat $1 | $SHELLSTATD_HOME/lib/python_send.py --server=$graphite_host --port=$graphite_port --protocol=$graphite_protocol --verbose=$verbose_logging &
-  		echo $! >> $PID_FILE 
+  		cat $1 | $SHELLSTATD_HOME/lib/python_send.py --server=$graphite_host --port=$graphite_port --protocol=$graphite_protocol --verbose=$verbose_logging
   	else
 		while IFS= read -r line; do
 			if [  $verbose_logging == "True"  ]; then
