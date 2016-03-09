4872e689a9ccdf8b535c940800c5cf7dd936ad21
jdbranham <jdbranham@hotmail.com>
2016-03-09 13:23:15 -0600

    added missing '$' for PAYLOAD var

--- lib/jolokia_request.sh
+++ lib/jolokia_request.sh
@@ -31,13 +31,13 @@ function jolokiaRequest {
 			while read -r value_entry; do
 				if [ ! "$value_entry" = "" ]; then
 					local full_string="$PREFIX$MBEAN_NAME$MBEAN_ATTRIBUTE.$value_entry $MBEAN_TIMESTAMP\n"
-					PAYLOAD+=("${full_string}")
+					$PAYLOAD+=("${full_string}")
 				fi
 			done < <(echo -e "$MBEAN_VALUE")
 		else
 			# The value is a number
 			local full_string="$PREFIX$MBEAN_NAME$MBEAN_ATTRIBUTE $MBEAN_VALUE $MBEAN_TIMESTAMP\n"
-			PAYLOAD+=("$(echo -e $full_string | sed -r 's/\n//g')") 
+			$PAYLOAD+=("$(echo -e $full_string | sed -r 's/\n//g')") 
 			
 		fi
 		for payload_item in "${PAYLOAD[*]}"; do
